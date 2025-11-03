8b1bb62b0ee43317a580de4dc8727121ddcae9dd
lz-souzx <luizadesouza140@gmail.com>
2025-11-03 11:49:38 -0300

    Create main.dart

---
+++ aula 5/main.dart
@@ -0,0 +1,26 @@
+import 'package:aula5/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aula  5',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHome(),
+    );
+  }
+}
+
