b9c07443517a56c6b9f8513230591aae8324d052
lz-souzx <luizadesouza140@gmail.com>
2025-11-17 11:56:22 -0300

    Add files via upload

---
+++ lib/screens/myhomepage.dart
@@ -0,0 +1,73 @@
+import 'package:aula6/model/profile.dart';
+import 'package:aula6/widget/mybottomnavigation.dart';
+import 'package:aula6/widget/mycard.dart';
+import 'package:aula6/widget/mydrawer.dart';
+import 'package:aula6/widget/mystory.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyHomePage extends StatelessWidget {
+  //criando lista de dados
+
+  List<Profile> listaDados = [
+    Profile(
+        name: "Jerry",
+        image:
+            "https://toppng.com/uploads/preview/jerry-tom-and-jerry-11530964889gtgzrtgom1.png",
+        like: true),
+    Profile(
+        name: "Marry",
+        image:
+            "https://i.pinimg.com/originals/99/78/37/99783765e86e91fccf099631edc3e8c5.png",
+        like: true),
+    Profile(name: "Claudia", image: "", like: false),
+    Profile(name: "Eduarda", image: "", like: true),
+    Profile(name: "Eduarda", image: "", like: true),
+    Profile(name: "Eduarda", image: "", like: true),
+    Profile(name: "Eduarda", image: "", like: true)
+  ];
+
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aula 6"),
+        backgroundColor: Color.fromARGB(255, 255, 170, 248),
+      ),
+      drawer: MyDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: listaDados.length,
+                  itemBuilder: (context, item) {
+                    return MyStory(
+                        title: listaDados[item].name,
+                        image: listaDados[item].image);
+                  }),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: listaDados.length,
+                  itemBuilder: (context, item) {
+                    return MyCard(
+                        title: listaDados[item].name,
+                        image: listaDados[item].image,
+                        like: listaDados[item].like);
+                  }),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: MyBottomNavigation(),
+    );
+  }
+}
---
+++ lib/widget/mystory.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyStory extends StatelessWidget {
+
+  final String? title;
+  final String? image;
+
+
+  const MyStory({super.key, required this.title, this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, right: 8, left: 8),
+      child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 40,
+          backgroundColor: Color.fromARGB(255, 243, 197, 212),
+          backgroundImage: NetworkImage(image!),
+          ),
+          Text(title!)
+        ],
+      ),
+    );
+  }
+}
