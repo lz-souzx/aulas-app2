a6f4f5e28f7593fc388386a1700ddaa146003c57
Luiza Souza <luizadesouza140@gmail.com>
2025-12-16 17:32:14 -0300

    aula 17

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:api_esq/pages/registros_page.dart';
 import 'package:flutter/material.dart';
-import 'package:form_flutter/myapp.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MainApp());
 }
 
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RegistrosPage()
+    );
+  }
+}
